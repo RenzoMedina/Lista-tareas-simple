d4ce5218ac6536fd51bb7978b5ba8135ba2f87af
RenzoMedina <medinast30@gmail.com>
2023-02-18 19:49:44 -0300

    he modifico un poco la vista, además he agregado un poco de logica para que se agregue una lista de tarea vamos a medio camino

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('se ha conectado')
+const task = document.getElementById('task')
+const btnTask = document.getElementById('btnTask')
+const cardFooter= document.getElementById('card-footer')
+
+
+btnTask.addEventListener('click', () =>{
+
+    addTask(task.value)
+} )
+
+function addTask(t){
+    let id = t
+    let icon = document.createElement('input')
+    icon.setAttribute('type', 'checkbox')
+    icon.setAttribute('id', `${id}`)
+    
+    let tas = document.createElement('label')
+    tas.setAttribute('for', `${id}`)
+
+
+    tas.innerText=t
+    cardFooter.appendChild(tas)
+    cardFooter.append(icon)
+
+    
+}
